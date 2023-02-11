5b6c401b197e02a8836b031f3369092f0125770a
Jon <deal4131@gmail.com>
2023-02-11 02:41:07 -0500

    adds peice spawn and insert logic

--- src/App.js
+++ src/App.js
@@ -1,55 +1,191 @@
-import logo from './logo.svg';
 import './App.css';
-import React, {useState} from "react";
+import React from "react";
 
 import GridSquare from "./GridSquare";
 
 
 
 
-function App() {
-  const [gameBoard] = useState(createGameBoard)
+class App extends React.Component {
+
+    constructor(props) {
+
+        super(props);
+        this.state = {
+            gameBoard: [],
+            activeColor: "none",
+            activePiece: [],
+            activeXPos: 4,
+            activeYPos: 10,
+            isGameInitilized: false,
 
 
-  function createGameBoard() {
-    const debugMode = false;
-    let gameBoard = [];
-    for (let height = 0; height < 20; height ++){
-      gameBoard.push(
+        };
+        this.createGameBoard = this.createGameBoard.bind(this);
+        this.spawnPiece = this.spawnPiece.bind(this);
+        this.tickGame = this.tickGame.bind(this);
+        this.insertPiece = this.insertPiece.bind(this);
 
-          <div className="RowPanel">
-            <GridSquare X={1} Y={height} debugMode={debugMode}> </GridSquare>
-            <GridSquare X={2} Y={height} debugMode={debugMode}> </GridSquare>
-            <GridSquare X={3} Y={height} debugMode={debugMode}></GridSquare>
-            <GridSquare X={4} Y={height} debugMode={debugMode}></GridSquare>
-            <GridSquare X={5} Y={height} debugMode={debugMode}> </GridSquare>
-            <GridSquare X={6} Y={height} debugMode={debugMode}></GridSquare>
-            <GridSquare X={7} Y={height} debugMode={debugMode}></GridSquare>
-            <GridSquare X={8} Y={height} debugMode={debugMode}></GridSquare>
-            <GridSquare X={9} Y={height} debugMode={debugMode}> </GridSquare>
-            <GridSquare X={10} Y={height} debugMode={debugMode}></GridSquare>
-
-          </div>
-      )
+         this.isActivePiece= false;
     }
 
-    return gameBoard
+
+   createGameBoard() {
+    let virturalGameboard = [];
+    for (let height = 0; height < 20; height ++){
+        virturalGameboard.push([]);
+    for (let width = 0; width < 10; width++){
+        virturalGameboard[height].push({backgroundColor: "grey"});
+    }
+
+    }
+
+    this.setState({gameBoard: virturalGameboard, isGameInitilized: true});
+  }
+     async spawnPiece(){
+
+        let pieceToSpawn = Math.floor(Math.random() * 7);
+
+        let shape = [];
+
+
+        if (pieceToSpawn === 0){
+             shape = [[1,1],[1,1]];
+            this.isActivePiece = true;
+            this.setState({ activePiece: shape, activeColor:"Yellow"}, this.insertPiece);
+
+        }else if(pieceToSpawn === 1) {
+            shape = [[1, 1, 1, 1]];
+            this.isActivePiece = true;
+            this.setState({ activePiece: shape, activeColor:"Cyan"}, this.insertPiece);
+        }else if(pieceToSpawn === 2) {
+            shape = [
+                [0,0,1],
+                [1,1,1],
+
+
+            ];
+            this.isActivePiece = true;
+            this.setState({ activePiece: shape, activeColor:"Orange"}, this.insertPiece);
+        }else if(pieceToSpawn === 3) {
+            shape = [
+                [0,1,1],
+                [1,1,0]
+
+            ];
+            this.isActivePiece = true;
+            this.setState({ activePiece: shape, activeColor:"Green"}, this.insertPiece);
+        }else if(pieceToSpawn === 4) {
+            shape = [
+                [0,1],
+                [1,1,1]
+
+
+            ];
+            this.isActivePiece = true;
+            this.setState({ activePiece: shape, activeColor:"Purple"}, this.insertPiece);
+        }else if(pieceToSpawn === 5) {
+            shape = [
+                [1],
+                [1,1,1]
+
+
+            ];
+            this.isActivePiece = true;
+            this.setState({ activePiece: shape, activeColor:"Blue"}, this.insertPiece);
+        }else if(pieceToSpawn === 6) {
+            shape = [
+                [1,1],
+                [0,1,1]
+
+
+            ];
+            this.isActivePiece = true;
+            this.setState({ activePiece: shape, activeColor:"Red"}, this.insertPiece);
+        }
+
+
+    }
+
+    insertPiece(){
+        const {activeYPos, activeXPos, gameBoard} =this.state;
+        if(this.state.activePiece === undefined){
+            debugger;
+        }
+
+        let localXOffset = 0;
+        let loacalYoffset = 0;
+        let localGameBoard =  JSON.parse(JSON.stringify(gameBoard));
+
+        this.state.activePiece.forEach((row) =>{
+            localXOffset =  0;
+            row.forEach((cell) =>{
+                if(cell === 1){
+
+                    localGameBoard[activeYPos + loacalYoffset][activeXPos + localXOffset].backgroundColor = this.state.activeColor;
+
+                }
+                localXOffset++;
+            })
+            loacalYoffset++;
+        })
+        this.setState({gameBoard: localGameBoard})
+
+    }
+
+
+    async tickGame(){
+      console.log("tick");
+
+
+          if (this.isActivePiece){
+
+          }else{
+              this.spawnPiece();
+
+
+          }
+
+
+
+
   }
 
-  function getGridSquareByCoordinates(){
-    let foundGridSquare = gameBoard;
-    console.log(foundGridSquare)
-  }
-  getGridSquareByCoordinates();
-  return (
-    <div>
-      <div className="OuterSquare">
-          {gameBoard}
+
+ componentDidMount() {
+     this.createGameBoard();
+    setInterval(this.tickGame, 5000)
+}
+
+    render(){
+        const {gameBoard} = this.state
 
 
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <div className="OuterSquare">
+
+                {
+                    gameBoard.map((rowElement )=>{
+
+                        return (
+                            <div className="RowPanel">
+                                {
+                                    rowElement.map((cellElement) =>{
+                                        return <div className={`InternalSquare ${cellElement.backgroundColor}`}> </div>
+                                    } )
+                                }
+                            </div>
+                        )
+                    })
+                }
+
+
+            </div>
+        </div>
+    );
+}
+
 }
 
 export default App;
--- src/GridSquare.js
+++ src/GridSquare.js
@@ -1,10 +1,11 @@
 import React, {useState} from "react";
 import App from "./App";
 
-function GridSquare({X, Y, DebugMode}) {
+function GridSquare({X, Y, DebugMode, Color}) {
     const [xLocation]= useState(X);
     const [yLocation]= useState(Y);
     const [debugMode]= useState(DebugMode)
+    const [color] =useState(Color)
 
     return(
             <div className="InternalSquare">{ debugMode ?  `${xLocation}, ${yLocation}`: ""} </div>
